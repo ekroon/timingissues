79fa39933fc903702db26ac57dddfe4cec6e7f55
RaymanX <raymond@k3n.nl>
2010-11-13 16:34:37 +0100

    werkende socket

---
+++ server/SimpleFileServer.js
@@ -0,0 +1,43 @@
+var sys = require("sys"),
+    http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs"),
+    io = require('socket.io');
+    
+var fileRoot = '/Users/raymond/prog/timingissues/frontend/';
+
+var server = http.createServer(function(request, response) {
+    var uri = url.parse(request.url).pathname;
+    var filename = path.join(fileRoot, uri);
+    path.exists(filename, function(exists) {
+    	if(!exists) {
+    		response.writeHead(404, {"Content-Type": "text/plain"});
+    		response.write("404 Not Found\n");
+    		response.end();
+    		return;
+    	}
+
+    	fs.readFile(filename, "binary", function(err, file) {
+    		if(err) {
+    			response.writeHead(500, {"Content-Type": "text/plain"});
+    			response.write(err + "\n");
+    			response.end();
+    			return;
+    		}
+
+    		response.writeHead(200);
+    		response.write(file, "binary");
+    		response.end();
+    	});
+    });
+});
+server.listen(8080);
+
+var socket = io.listen(server);
+socket.on('connection', function(client) {
+	var message = {type : 'score-update', data : {home : 1, away: 2}}; 
+	setTimeout(function() {client.send(JSON.stringify(message));}, 5000);
+});
+
+sys.puts("Server running at http://localhost:8080/");
