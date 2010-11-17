4af1d56cfd4db948f1a6ffddbe0fa176093c6335
Erwin Kroon <erwin@k3n.nl>
2010-11-17 23:14:21 +0100

    Added support for external configuration

--- server/SimpleFileServer.js
+++ server/SimpleFileServer.js
@@ -4,12 +4,12 @@ var sys = require("sys"),
     path = require("path"),
     fs = require("fs"),
     io = require('socket.io');
+var config = require(__dirname+'/../config/configuration.js');
 
 var fileRoot = __dirname + '/../frontend';
 var portNumber = parseInt(process.argv[2], 10);
 if ('NaN' == portNumber.toString()) {
-    sys.puts("Portnumber not given, defaulting to 8080");
-    portNumber = 8080;
+    portNumber = config.portNumber || 8080;
 }
 
 var server = http.createServer(function(request, response) {
@@ -37,8 +37,15 @@ var server = http.createServer(function(request, response) {
         });
     });
 });
-server.listen(portNumber, function() {
-    sys.puts("Server running at http://localhost:"+portNumber+"/");
+
+// safety check for services already listening on localhost only
+if ((config.listen || null) == null) {
+    server.listen(portNumber, 'localhost');
+    server.close();
+}
+
+server.listen(portNumber, config.listen || null, function() {
+    sys.puts("Server running at http://"+(config.listen || "INADDR_ANY")+":"+portNumber+"/");
     var socket = io.listen(server);
     socket.on('connection', function(client) {
         var message = {type : 'score-update', data : {home : 1, away: 2}};
