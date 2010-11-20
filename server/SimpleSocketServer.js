728654f50182cbca66bcdeb3ac1b4ebf71062fd4
Erwin Kroon <erwin@k3n.nl>
2010-11-20 13:01:16 +0100

    Deleted old servers

--- server/SimpleSocketServer.js
+++
@@ -1,7 +0,0 @@
-var net = require('net');
-net.createServer(function (socket) {
-  socket.write("Echo server\r\n");
-  socket.on("data", function (data) {
-    socket.write(data);
-  });
-}).listen(8124, "127.0.0.1");
